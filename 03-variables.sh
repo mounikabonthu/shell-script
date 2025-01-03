6bfee4bc7c548840b9da116555408b0e66e4bd64
mounikabonthu <bontumonika@gmail.com>
2025-01-03 14:34:35 +0530

    variables added

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This is a conversation
+
+PERSON1=Soni
+PERSON2=Moni
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2: Hi $PERSON1"
+echo "$PERSON1: How are you doing?"
+echo "$PERSON2: I am good. How are you?"
+echo "whats going on"
