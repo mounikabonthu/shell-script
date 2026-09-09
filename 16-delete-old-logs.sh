1db28fa51f45fd2d6ff684c609f2756f79791c2a
mounikabonthu <bontumonika@gmail.com>
2026-09-09 21:51:55 +0530

    shellscript

---
+++ 16-delete-old-logs.sh
@@ -0,0 +1,12 @@
+!/bin/bash
+SOURCE_DIR="/home/ec2-user/app-logs"
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+echo "Files to be deleted: $FILES_TO_DELETE"
+
+# while read -r filepath # here filepath is the variable name, you can give any name
+# do
+#     echo "Deleting file: $filepath" &>>$LOG_FILE_NAME
+#     rm -rf $filepath
+# echo "Deleted file: $filepath"
+# done <<< $FILES_TO_DELETE 
