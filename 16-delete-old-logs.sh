a02ed6ee382612fea29a442167bc85e5c120eb3c
mounikabonthu <bontumonika@gmail.com>
2026-09-09 22:08:12 +0530

    shellscript

--- 16-delete-old-logs.sh
+++ 16-delete-old-logs.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
+
 SOURCE_DIR="/home/ec2-user/app-logs"
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 echo "Files to be deleted: $FILES_TO_DELETE"
 
 while read -r filepath # here filepath is the variable name, you can give any name
 do
-    echo "Deleting file: $filepath" 
+    echo "Deleting file: $filepath" &>>$LOG_FILE_NAME
     #rm -rf $filepath
-#echo "Deleted file: $filepath"
-done <<< $FILES_TO_DELETE 
+    #echo "Deleted file: $filepath"
+done <<< $FILES_TO_DELETE
