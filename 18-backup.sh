accd2947c0430d1db7ad958db6aa8eecf3ee60e0
mounikabonthu <bontumonika@gmail.com>
2026-09-14 11:43:13 +0530

    backup old logs

--- 18-backup.sh
+++ 18-backup.sh
@@ -19,7 +19,7 @@ USAGE(){
     echo -e "$R USAGE:: $N backup <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
     exit 1
 }
-echo " logfile name is $LOG_FILE"
+#echo " logfile name is $LOG_FILE"  #for my testing
 
 if [ $# -lt 2 ]
 then
