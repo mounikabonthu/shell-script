29fb27afa3ddba97d0fed16416bb1369530f05a5
mounikabonthu <bontumonika@gmail.com>
2026-09-14 11:26:44 +0530

    backup old logs

--- 17-while.sh
+++ 17-while.sh
@@ -4,3 +4,5 @@ while read -r line
 do
     echo $line
 done < 16-delete-old-logs.sh
+
+#output is your code for the script 16-delete-old-logs.sh prints
---
+++ 18-backup.sh
@@ -0,0 +1,26 @@
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} # if user is not providing number of days, we are taking 14 as default
+mkdir -p /home/ec2-user/shellscript-logs
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+USAGE(){
+    #echo -e "$R USAGE:: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
+    echo -e "$R USAGE:: $N backup <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
+    exit 1
+}
+
+if [ $# -lt 2 ]
+then
+    USAGE
+fi
