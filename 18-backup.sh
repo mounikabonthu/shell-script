cd64f7662c1a161af3cc538e33485d52353f5ea6
mounikabonthu <bontumonika@gmail.com>
2026-09-14 11:29:27 +0530

    backup old logs

--- 18-backup.sh
+++ 18-backup.sh
@@ -24,3 +24,14 @@ if [ $# -lt 2 ]
 then
     USAGE
 fi
+if [ ! -d "$SOURCE_DIR" ]
+then
+    echo -e "$SOURCE_DIR Does not exist...Please check"
+    exit 1
+fi
+
+if [ ! -d "$DEST_DIR" ]
+then
+    echo -e "$DEST_DIR Does not exist...Please check"
+    exit 1
+fi
